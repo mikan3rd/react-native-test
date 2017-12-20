e0e6f22d0a8b39256616367e80dc17da78ace4a8
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-12-20 23:25:22 +0900

    Linkingの追加

--- app/components/SpotCard.js
+++ app/components/SpotCard.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import {StyleSheet, Text, View, ScrollView, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, ScrollView, TouchableOpacity, WebView, Linking} from 'react-native';
 import Svg, {Use, Image} from 'react-native-svg';
 
 class SpotCard extends React.Component {
 
-    openMap() {
-        console.log("success");
+    openMap(address) {
+        const url = `https://www.google.co.jp/maps?q=${address}`;
+        Linking.openURL(url).catch(err => console.error('An error occurred', err));
+    }
+
+    openPathee(spotId) {
+        const url = `https://pathee.com/spots/${spotId}`;
+        Linking.openURL(url).catch(err => console.error('An error occurred', err));
     }
 
   render() {
-    console.log(this.props.name);
+    console.log(this.props.address);
     const businessHour = this.props.businessHour ? (<Text>{this.props.businessHour}</Text>) : null;
     const building = this.props.building ? (<Text>{this.props.building}</Text>) : null;
+    const uri = 'http://stackoverflow.com/questions/35531679/react-native-open-links-in-browser';
     return (
       <View style={styles.spot}>
         <View className="spot__left" style={styles.spotLeft}>
+            <TouchableOpacity
+                onPress={() => this.openPathee(this.props.spot_id)}
+            >
           <Text style={styles.shopName}>{this.props.name}</Text>
           <Text>{this.props.keywords.join(" ")}</Text>
           {businessHour}
@@ -28,12 +38,13 @@ class SpotCard extends React.Component {
               <Text />
             </View>
           </View>
+          </TouchableOpacity>
         </View>
         <View className="spot__right" style={styles.spotRight}>
           <View className="spot__right__container">
             <View className="spot__right__container__action">
             <TouchableOpacity
-                onPress={this.openMap}
+                onPress={() => this.openMap(this.props.address)}
             >
                 <Text style={styles.mapText}>距離: {this.props.distance}m</Text>
                 <Text style={styles.mapText}>地図を表示</Text>
